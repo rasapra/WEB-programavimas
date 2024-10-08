52e2d126a0e0c3f38f2d1abe4cb5b8f7f96cdc2f
rasapra <rasiukaspr@gmail.com>
2024-10-08 17:52:27 +0300

    I am learning

---
+++ 13 paskaita_javascript/script.js
@@ -0,0 +1,26 @@
+console.log("Hello my name is Rasa"); // This is my test code
+
+const firstName = "Rasa";
+const lastName = "Praninskienė";
+
+const fullName = firstName + " " + lastName;
+
+console.log(firstName + " " + lastName);
+
+// arba
+
+console.log(fullName)
+
+let itemZ
+console.log(itemZ);
+
+itemZ = "Vėliau priskirta reikšmė"
+console.log(itemZ);
+
+// 1 užduotis
+
+const shopName = "Iki";
+const peopleCount = 300;
+const consentWithPrivacyPolicy = true;
+
+const test = "Rasa";
