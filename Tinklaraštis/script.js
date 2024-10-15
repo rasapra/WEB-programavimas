fe70e450f26ae2707026dc38efa0b921d82b4f3c
rasapra <rasiukaspr@gmail.com>
2024-10-15 17:12:04 +0300

    I changed code

--- Tinklaraštis/script.js
+++ Tinklaraštis/script.js
@@ -1,5 +1,3 @@
 document.getElementById("contact-form").addEventListener("submit", function(event) {
-    // Šiuo atveju, jeigu naudojate `action` formoje, nebūtina, jei siuntimas vyks į serverį.
-    // event.preventDefault(); 
     alert("Dėkoju už Tavo žinutę!"); 
 });
